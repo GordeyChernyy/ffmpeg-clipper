515f9f0284ed9a8b71ccf84a4593b1b68a66095e
Gordey Chernyy <kewava@gmail.com>
2021-01-03 19:43:27 -0500

    add sequence

--- Editor/Scripts/ClipperArgs/ClipperArgs.cs
+++ Editor/Scripts/ClipperArgs/ClipperArgs.cs
@@ -7,5 +7,10 @@ namespace ffmpegClipper
     public abstract class ClipperArgs : ScriptableObject
     {
         public abstract string Args { get; }
+
+        [ContextMenu("PrintArgs")]
+        public void PrintArgs() {
+            Debug.Log(Args);
+        }
     }
 }
--- Editor/Scripts/ClipperArgs/CustomArgs.cs
+++ Editor/Scripts/ClipperArgs/CustomArgs.cs
@@ -1,11 +1,43 @@
-﻿using UnityEngine;
+﻿using ScriptableVariables;
+using System;
+using UnityEngine;
 
 namespace ffmpegClipper
 {
     [CreateAssetMenu(menuName = "ffmpeg Clipper/CustomArgs")]
     class CustomArgs : ClipperArgs
     {
+        public SString[] variables;
+
         public string args;
-        public override string Args => args;
+        public override string Args => ArgsWithVariables();
+
+        [ContextMenu("Verify")]
+        public void Verify()
+        {
+            Debug.Log(Args);
+        }
+
+        private string ArgsWithVariables()
+        {
+            var result = args.Split(new[] { "{", "}" }, StringSplitOptions.None);
+
+            int i = 0;
+            string output = "";
+            foreach (var r in result)
+            {
+                if (i % 2 != 0)
+                {
+                    int.TryParse(r, out int index);
+                    output += variables[index].value;
+                }
+                else
+                {
+                    output += r;
+                }
+                i++;
+            }
+            return output;
+        }
     }
 }
