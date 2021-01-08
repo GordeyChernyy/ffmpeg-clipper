660d7f64ad06d12a5ab255c7f824b26e1eef56ca
Gordey Chernyy <kewava@gmail.com>
2021-01-08 12:03:42 -0500

    v0.0.2 add app version

---
+++ Assets/ffmpegClipper/Editor/Scripts/ClipperArgs/AppVersion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace ffmpegClipper
+{
+    [CreateAssetMenu(menuName = "ffmpeg Clipper/AppVersion")]
+    class AppVersion : ClipperArgs
+    {
+        public override string Args => PlayerSettings.bundleVersion;
+    }
+}
